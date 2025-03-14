e29d836ab5af5c3d1fab7b4c56453daeadc34976
norm095 <norman.aga095@gmail.com>
2025-03-14 17:02:21 +0100

    form_template

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        return f"Hello, {name}!"
+    return render_template("form.html")
 
 @app.route("/about")
 def about():
     return "This is the about page!"
+
 
 
 
